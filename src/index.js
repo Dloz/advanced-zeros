beb9ab4bb2f2489dc5d6fd78a58c0d1621e14618
Dloz <lozovik.dima@mail.ru>
2018-09-22 18:29:38 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var result = 0;
+  var primes = findPrimeFactors(base);
+  var primeKeys = Object.keys(primes);
+
+  for(let i = 0; i < primeKeys.length; i++){
+    var diver = primeKeys[i];
+    var degree =  primes[primeKeys[i]];
+    var tempRes = 0;
+
+    while(diver < number){
+      tempRes += Math.floor(number/diver);
+      diver *= primeKeys[i];
+    }
+    
+    tempRes = Math.floor(tempRes/degree);
+    
+    if(result){
+      if(tempRes <= result){
+        result = tempRes; 
+      } 
+    }else{
+      result = tempRes;
+    }
+  }
+
+  return result;
+}
+
+
+function findPrimeFactors(number){
+  var arrOfPrime = [];
+  for(let i = 2; i <= number; i++){
+    while(number % i === 0){
+      arrOfPrime.push(i);
+      number = number/i;
+    }
+  }
+
+  var res = findNumberOfMax(arrOfPrime);
+  
+  return res;
+}
+
+function findNumberOfMax(array){
+  var map = {};
+
+  for(let i = 0; i < array.length; i++){
+    if(map[array[i]]){
+      map[array[i]]++;
+    } else {
+      map[array[i]] = 1;
+    }
+  }
+  return map;
 }
